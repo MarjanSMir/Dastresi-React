7b80f31f109749aa23e1914f3655f06a811afeba
MarjanSmir <Marjansmirmohammad@outlook.com>
2024-11-04 12:03:04 +0330

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home/Home";
+import RouterAdmin from "../../pages/Admin/Admin";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Admin/*" element={<RouterAdmin/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,6 @@
+const Admin = () =>{
+    return (
+        <h1 className="">Admin</h1>
+    )
+}
+export default Admin;
